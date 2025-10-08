410c8e257efb5e9fcbb7bfad77a72d895e668f00
0x1m4o <chuntdevnx@gmail.com>
2025-10-08 16:14:31 +0700

    feat(schema): create database schema

---
+++ backend/prisma/migrations/20251008090438_create_schemas_table/migration.sql
@@ -0,0 +1,85 @@
+-- CreateEnum
+CREATE TYPE "DisputeBy" AS ENUM ('BRAND', 'CREATOR');
+
+-- CreateEnum
+CREATE TYPE "ContractStatus" AS ENUM ('ACTIVE', 'PENDING_REVIEW', 'DISPUTED', 'COMPLETED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "DisputeChoice" AS ENUM ('80/20', '0/100');
+
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'RELEASE', 'REFUND');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "VerificationResult" AS ENUM ('ON_TIME', 'LATE', 'DELETED', 'NOT_FOUND');
+
+-- CreateTable
+CREATE TABLE "contracts" (
+    "id" TEXT NOT NULL,
+    "walletBrand" TEXT NOT NULL,
+    "walletCreator" TEXT NOT NULL,
+    "emailBrand" TEXT NOT NULL,
+    "emailCreator" TEXT NOT NULL,
+    "social" TEXT NOT NULL,
+    "amount" DECIMAL(20,2) NOT NULL,
+    "brief" TEXT NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "status" "ContractStatus" NOT NULL DEFAULT 'PENDING_REVIEW',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "contracts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verification_logs" (
+    "id" SERIAL NOT NULL,
+    "contractId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "result" "VerificationResult" NOT NULL,
+    "verifiedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "contentTimestamp" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "verification_logs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "disputes" (
+    "id" SERIAL NOT NULL,
+    "contractId" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "evidence" TEXT NOT NULL,
+    "disputedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "resolvedAt" TIMESTAMP(3),
+    "disputedBy" "DisputeBy" NOT NULL,
+    "creatorChoice" "DisputeChoice",
+
+    CONSTRAINT "disputes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" SERIAL NOT NULL,
+    "contractId" TEXT NOT NULL,
+    "amount" DECIMAL(20,2) NOT NULL,
+    "txHash" TEXT,
+    "type" "TransactionType" NOT NULL,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contracts_id_key" ON "contracts"("id");
+
+-- AddForeignKey
+ALTER TABLE "verification_logs" ADD CONSTRAINT "verification_logs_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "contracts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "disputes" ADD CONSTRAINT "disputes_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "contracts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "contracts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
